4760847cb0abd74f114269b2075f4c1f97a13acc
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2022-04-12 10:16:07 +0530

    REST API configured

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 #import flask module
 from flask import Flask
+from flask_restful import Api
+from flask import Flask
+from resources.routes import initialize_routes
+
 
 app = Flask(__name__)
+api = Api(app)
 
 #Test route
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+#initialize all the routes
+initialize_routes(api)
 
 #Main function
 if __name__ == '__main__':
---
+++ resources/speechExtraction.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource
+import speech_recognition as sr
+import moviepy.editor as mp
+
+class SpeechExtraction(Resource):
+    def get(self):
+        
+        return 'API is working'
